bcd7694f42e8b13997a584d8166c24dbb7232987
DogoCaraballo <6132401+DogoCaraballo@users.noreply.github.com>
2019-05-15 00:11:36 -0300

    Create braclose.py

---
+++ braclose.py
@@ -0,0 +1,40 @@
+TRAMPA = -1
+RESULTADO_ACEPTADO = "ACEPTADO"
+RESULTADO_TRAMPA = "TRAMPA"
+RESULTADO_NO_ACEPTADO = "NO_ACEPTADO"
+
+#delta braclose
+def d_braclose(estado_anterior, caracter):
+	if estado_anterior == 0 and caracter == "}":
+		return 1
+
+	
+	return TRAMPA
+
+#automata braclose
+def a_braclose(cadena):
+	Finales = [1]
+	estado_actual = 0
+
+	for caracter in cadena:
+		estado_proximo = d_braclose(estado_actual, caracter)
+		if estado_proximo == TRAMPA:
+			return RESULTADO_TRAMPA
+		estado_actual = estado_proximo
+	
+	if estado_actual in Finales:
+		return RESULTADO_ACEPTADO
+	else:
+		return RESULTADO_NO_ACEPTADO
+
+## PRUEBAS
+
+casos = [
+	("}", RESULTADO_ACEPTADO),
+	("", RESULTADO_NO_ACEPTADO),
+	("d", RESULTADO_TRAMPA),
+]
+
+for cadena, resultado in casos:
+	print((cadena, a_braclose(cadena)))
+	print( a_braclose(cadena) == resultado)
