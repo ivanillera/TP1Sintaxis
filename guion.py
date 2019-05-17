2a9eec5bf4b0c7bdcf4c670c43f5d21945776daa
nfernandez8 <46354556+nfernandez8@users.noreply.github.com>
2019-05-17 17:25:47 -0300

    Update guion.py

--- guion.py
+++ guion.py
@@ -3,36 +3,41 @@ RESULTADO_ACEPTADO = "ACEPTADO"
 RESULTADO_TRAMPA = "TRAMPA"
 RESULTADO_NO_ACEPTADO = "NO_ACEPTADO"
 
+
 #delta "-"
 def d_guion(estado_anterior, caracter):
-	if estado_anterior == 0 and caracter == "-":
-		return 1
-  return TRAMPA
+    if estado_anterior == 0 and caracter == "-":
+        return 1
+
+    
+    return TRAMPA
 
 #automata "-"
 def a_guion(cadena):
-	Finales = [1]
-	estado_actual = 0
+    Finales = [1]
+    estado_actual = 0
 
-	for caracter in cadena:
-		estado_proximo = d_guion(estado_actual, caracter)
-		if estado_proximo == TRAMPA:
-			return RESULTADO_TRAMPA
-		estado_actual = estado_proximo
-	
-	if estado_actual in Finales:
-		return RESULTADO_ACEPTADO
-	else:
-		return RESULTADO_NO_ACEPTADO
+    for caracter in cadena:
+        estado_proximo = d_guion(estado_actual, caracter)
+        if estado_proximo == TRAMPA:
+            return RESULTADO_TRAMPA
+        estado_actual = estado_proximo
+    
+    if estado_actual in Finales:
+        return RESULTADO_ACEPTADO
+    else:
+        return RESULTADO_NO_ACEPTADO
 
 ## PRUEBAS
 
 casos = [
-	("-", RESULTADO_ACEPTADO),
-	("", RESULTADO_NO_ACEPTADO),
-	("'", RESULTADO_TRAMPA),
+    ("-", RESULTADO_ACEPTADO),
+    ("", RESULTADO_NO_ACEPTADO),
+    (",", RESULTADO_TRAMPA),
+    ("; ", RESULTADO_TRAMPA),
+    ("a;", RESULTADO_TRAMPA)
 ]
 
 for cadena, resultado in casos:
-	print((cadena, a_guion(cadena)))
-	print( a_guion(cadena) == resultado)
+    print((cadena, a_guion(cadena)))
+    assert a_guion(cadena) == resultado
